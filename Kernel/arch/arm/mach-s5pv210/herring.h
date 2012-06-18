a1aede948a25cc68789a62a32c74f2ed060d0613
Shareef Ali <shareefalis@gmail.com>
2012-06-18 01:56:31 -0500

    Move to Samsung Nexus S 4G RIL (WIP) DO NOT COMMIT
    Works in 3.0 but not in 2.6 kernel
    I think we are missing CDMA bits because this kernel source was worked on before NS4G sources.
    patchset 2: added cdma stuff
    patch set3: fixed support in 2.6 kernel
    Change-Id: I783bf857459a01b7bb155757da7d391bddb2581f

--- Kernel/arch/arm/mach-s5pv210/herring.h
+++ Kernel/arch/arm/mach-s5pv210/herring.h
@@ -10,6 +10,9 @@ struct uart_port;
 void herring_bt_uart_wake_peer(struct uart_port *port);
 extern void s3c_setup_uart_cfg_gpio(unsigned char port);
 
+#define is_cdma_wimax_dev() (!!((system_rev & 0xFFF0) == 0x20))
+#define is_cdma_wimax_rev0() (!!((system_rev & 0xFFFF) == 0x20))
+
 extern struct s5p_panel_data herring_panel_data;
 
 #endif
